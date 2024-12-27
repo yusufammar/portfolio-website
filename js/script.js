300ba799230926e8bf2f6003aa4f4c2e1380d23e
yusufammar <yousifammar.ya@gmail.com>
2024-12-27 15:09:08 +0100

    feat: Add recommendation section

--- js/script.js
+++ js/script.js
@@ -1,58 +1,93 @@
-let skillsList= [
-    {"Skill": "HTML","Years of Experience": 2, "imgPath": "img/html.png"},
-    {"Skill": "CSS","Years of Experience": 2, "imgPath": "img/css.png"},
-    {"Skill": "JS","Years of Experience": 1.5, "imgPath": "img/js.png"},
-    {"Skill": "React","Years of Experience": 1, "imgPath": "img/react.png"},
-    {"Skill": "Node.js","Years of Experience": 1, "imgPath": "img/node.png"},
-    {"Skill": "Java","Years of Experience": .5, "imgPath": "img/java.png"}
+let skillsList = [
+    { "Skill": "HTML", "Years of Experience": 2, "imgPath": "img/html.png" },
+    { "Skill": "CSS", "Years of Experience": 2, "imgPath": "img/css.png" },
+    { "Skill": "JS", "Years of Experience": 1.5, "imgPath": "img/js.png" },
+    { "Skill": "React", "Years of Experience": 1, "imgPath": "img/react.png" },
+    { "Skill": "Node.js", "Years of Experience": 1, "imgPath": "img/node.png" },
+    { "Skill": "Java", "Years of Experience": .5, "imgPath": "img/java.png" }
 ];
 
-let projectsList=[
-    {"Title": "Chatbot", "Description": "Developed a secure website integrated with chatbot for an automobile client using HTML, CSS, JavaScript and IBM Watson Assistant."},
-    {"Title": "Sentiment Analyzer", "Description": "Developed and deployed a sentiment analyzer for the box reviews section of an eCommerce platform using IBM NLU"},
-    {"Title": "Fashion Website", "Description": "Created a styled multi-page website for a new player in the fashion industry and integrated it with a shopping cart, using stripe for payment gateway"}, 
+let projectsList = [
+    { "Title": "Chatbot", "Description": "Developed a secure website integrated with chatbot for an automobile client using HTML, CSS, JavaScript and IBM Watson Assistant." },
+    { "Title": "Sentiment Analyzer", "Description": "Developed and deployed a sentiment analyzer for the box reviews section of an eCommerce platform using IBM NLU" },
+    { "Title": "Fashion Website", "Description": "Created a styled multi-page website for a new player in the fashion industry and integrated it with a shopping cart, using stripe for payment gateway" },
 ]
 
-let skillsListDiv= document.getElementById("skillsListDiv");
-let projectsListDiv= document.getElementById("projectsListDiv");
+let recommendationsList = [
+    "Jane is a very quick learner and quickly grasps key concepts of Web development. She got a great attitude & she is an excellent team player. She has a curious mind and asks the right question. She takes initiative within a team and has potentials to lead the team. ", 
+    " Working with Jane has been an awesome experience. She is highly knowledgable and always goes the extra step to make sure everything is right. For any future projects that need her expertise I would definitely want to work with her again. ", 
+    "I had worked along with Jane during the initial phase of our venture which needed Web development. She is a committed resource who has in depth knowledge about the domain. She will be an asset for any organisation! "
+];
 
 
-for (let i=0;i<skillsList.length;i++){
-    let skillBox = document.createElement('div')
-    skillBox.classList.add('skillBox');
-    
-    skillBox.innerHTML = `<img src= ${skillsList[i]["imgPath"]} class="skillIcon">`;
-    skillBox.innerHTML+= `<h3> ${skillsList[i]["Skill"]} </h3>`;
-    skillBox.innerHTML+= `<h3> ${skillsList[i]["Years of Experience"]} Years Experience </h3>`;
-    
-    skillsListDiv.appendChild(skillBox);
+populateSkillsList();
+populateProjectsList();
+populateRecommendationsList();
+
+
+function populateSkillsList() {
+    let skillsListDiv = document.getElementById("skillsListDiv");
+
+    for (let i = 0; i < skillsList.length; i++) {
+        let skillBox = document.createElement('div')
+        skillBox.classList.add('skillBox');
+
+        skillBox.innerHTML = `<img src= ${skillsList[i]["imgPath"]} class="skillIcon">`;
+        skillBox.innerHTML += `<h3> ${skillsList[i]["Skill"]} </h3>`;
+        skillBox.innerHTML += `<h3> ${skillsList[i]["Years of Experience"]} Years Experience </h3>`;
+
+        skillsListDiv.appendChild(skillBox);
+    }
 }
 
 
-for (let i=0;i<projectsList.length;i++){
-    let projectBox = document.createElement('div')
-    projectBox.classList.add('ProjectBox');
-    
-    
-    projectBox.innerHTML+= `<h2> ${projectsList[i]["Title"]} </h>`;
+function populateProjectsList() {
+    let projectsListDiv = document.getElementById("projectsListDiv");
 
-    let unorderedListElement= document.createElement("ul");
+    for (let i = 0; i < projectsList.length; i++) {
+        let projectBox = document.createElement('div')
+        projectBox.classList.add('ProjectBox');
 
 
-    let descriptionList= projectsList[i]["Description"].split(". ");
+        projectBox.innerHTML += `<h2> ${projectsList[i]["Title"]} </h>`;
+        projectBox.appendChild(returnDescription(i));
 
-        for (let j=0; j<descriptionList.length;j++){
-            unorderedListElement.innerHTML+=  `<li>${descriptionList[j]}</li>`
-        }
-           
-    projectBox.appendChild(unorderedListElement);
+        if (i < projectsList.length - 1)
+            projectBox.innerHTML += '<hr class="shortDivider">';
 
-    if (i<projectsList.length-1)
+        projectsListDiv.appendChild(projectBox);
+    }
+
+}
+
+function populateRecommendationsList() {
+    let recommendationsListDiv = document.getElementById("recommendationsListDiv");
+
+    for (let i = 0; i < recommendationsList.length; i++) {
+        let recommendationBox = document.createElement('div')
+        recommendationBox.classList.add('recommendationBox');
+
+        recommendationBox.innerHTML = `<i class='recommendationText'>"${recommendationsList[i]}" </i>`;
       
-    projectBox.innerHTML+= '<hr class="shortDivider">';
-    
-    projectsListDiv.appendChild(projectBox);
+
+        recommendationsListDiv.appendChild(recommendationBox);
+    }
 }
+
+
+function returnDescription(index) {
+    let unorderedListElement = document.createElement("ul");
+
+
+    let descriptionList = projectsList[index]["Description"].split(". ");
+
+    for (let j = 0; j < descriptionList.length; j++) {
+        unorderedListElement.innerHTML += `<li>${descriptionList[j]}</li>`
+    }
+
+    return unorderedListElement;
+}
+
 
 
 
